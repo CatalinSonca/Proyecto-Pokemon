edaee9c485e5e4281a7fe19c0895a6d3760e6954
CatalinSonca <catalin.andrei.sonca@bootcamp-upgrade.com>
2023-11-23 21:42:15 +0100

    modified:   resources/scripts.js

--- resources/scripts.js
+++ resources/scripts.js
@@ -2,7 +2,7 @@ const listaPokemon = document.querySelector("#listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i = 1; i <= 151; i++) {
+for (let i = 1; i <= 4000; i++) {
     fetch(URL + i)
         .then((response) => response.json())
         .then(data => mostrarPokemon(data))
@@ -53,7 +53,7 @@ botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
 
     listaPokemon.innerHTML = "";
 
-    for (let i = 1; i <= 151; i++) {
+    for (let i = 1; i <= 4000; i++) {
         fetch(URL + i)
             .then((response) => response.json())
             .then(data => {
